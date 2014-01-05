f857bda338aa1911880c38f9e4dc230dffd6b819
Tiago Fael Matos <KTachyon@gmail.com>
2014-01-05 16:38:41 +0000

    Initial Commit

---
+++ FastPrimeSieve/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FastPrimeSieve
+//
+//  Created by KTachyon on 05/01/14.
+//  Copyright (c) 2014 KTachyon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
